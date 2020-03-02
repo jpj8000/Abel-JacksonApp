f463110c4f4d146824d786021d490437315b2220
Abel B Rodas <ar73428@srcs.loc>
2020-03-02 10:45:42 -0800

    new2

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-function daysTill(dateString) {
-  var ch = new Date(dateString)
-  var today = new Date();
-  var diff = ch.getTime() - today.getTime();
-  var getTime = Math.ceil (diff/1000/3600/24);
-  return getTime;
+var checkButton = document.getElementById('button');
+document.getElementById('dateString').value
+if(checkButton){
+checkButton.addEventListener('click', daysTill);
 }
+
+function daysTill(dateString) {
+    var ch = new Date(document.getElementById('dateString').value);
+    var today = new Date();
+    var diff = ch.getTime() - today.getTime();
+    var getTime = Math.ceil(diff / 1000 / 3600 / 24);
+    //return getTime;
+    var display = document.getElementById('days').textContent = getTime;
+}
+
